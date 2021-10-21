f483769b7df3604786cf1b14fd86a05a8912e51b
georg <georgiy.reus@gmail.com>
2021-10-21 17:09:10 +0300

    3 - control point - refactoring - added css and map for scss

--- src/Components/HashTag.jsx
+++ src/Components/HashTag.jsx
@@ -1,24 +1,24 @@
 import React from "react";
 
 const HashTag = (props) => {
+    const note = props.note;
 
-    return (
-        props.note.map(item => {
-            let className = " ";
-            if (props.isActive) {
-                className = "active";
-            }
-            return (
-                <div key={item}>
-                    <div className={"tags"}>
-                        <div className={className}>{item}</div>
-                        <button onClick={() => props.delHashtag(item)} className="delTag">
-                            Удалить тэг
-                        </button>
-                    </div>
+    const listTag = note.map((item) => {
+        let className = " ";
+        if (props.isActive) {
+            className = "active";
+        }
+        return (
+            <div  key={item}>
+                <div className={"tags"}>
+                    <div className={className}>{item}</div>
+                    <button onClick={() => props.delHashtag(item)} className="delTag">
+                        Удалить тэг
+                    </button>
                 </div>
-            )
-        })
-    )
-}
+            </div>
+        );
+    });
+    return <div>{listTag}</div>
+};
 export default HashTag;
